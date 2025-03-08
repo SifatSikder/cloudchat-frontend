89b428185c03cd2c32e25a92257b1a77b8b721aa
sifat <bsse1221@iit.du.ac.bd>
2025-03-08 19:16:18 +0600

    Added FileHeader Section in cloudchat

--- src/components/addFile/AddFile.tsx
+++ src/components/addFile/AddFile.tsx
@@ -5,13 +5,10 @@ import AddFileHeader from "@/components/addFile/AddFileHeader.tsx";
 const AddFile = () => {
     return (
         <>
-            <div className=" border border-orange-500 w-auto h-auto rounded-[100px] py-2 px-3 bg-[#242424] flex justify-between items-center gap-3">
+            <div className="w-auto h-auto rounded-[100px] py-2 px-3 bg-[#242424] flex justify-between items-center gap-3">
                 <UserInfo/>
             </div>
-            <div className="border border-blue-600">
-                <AddFileHeader/>
-            </div>
-
+            <div><AddFileHeader/></div>
         </>
     );
 };
--- src/components/addFile/AddFileHeader.tsx
+++ src/components/addFile/AddFileHeader.tsx
@@ -1,10 +1,22 @@
 import * as React from "react";
+import {ReactComponent as CloudChatIcon} from "@/assets/icons/appIcon.svg"
 
 const AddFileHeader = ()=>{
     return (
-        <div>
-            Add Files to
-        </div>
+        <>
+            <div className="gap-0.5 flex items-center justify-center text-white font-normal text-[54px] leading-[100%] tracking-[-3%] text-center align-middle">
+                <span>Add Files to</span>
+                <span><CloudChatIcon/>
+                </span> Cloud Chat <span/>
+            </div>
+            <div className="flex flex-col items-center justify-between">
+                <span className="font-normal text-[14px] leading-[130%] tracking-[1%] text-center align-middle text-[#B7B7B7]">Your files will not be stored on our servers and no AI models will be trained.</span>
+                <span className="font-semibold text-[14px] leading-[130%] tracking-[1%] text-center align-middle text-[#F37F0C]">Supported File Types: .docx, .pdf, .epub, and many text filetypes</span>
+            </div>
+
+
+        </>
+
     )
 }
 
