fe90032648f840f4e9a1ad9fe7e24a1148df554d
sifat <bsse1221@iit.du.ac.bd>
2025-03-08 21:40:43 +0600

    added the attached file

--- src/components/ui/attachedFiles.tsx
+++ src/components/ui/attachedFiles.tsx
@@ -2,6 +2,7 @@ import React, { FC, SVGProps } from "react";
 import {Checkbox as Check} from "@/components/ui/checkbox.tsx";
 
 import {ReactComponent as Info} from "@/assets/icons/info.svg"
+import {ReactComponent as Cross} from "@/assets/icons/cross.svg"
 
 interface AttachedFileProps {
     fileIcon: FC<SVGProps<SVGSVGElement>>;
@@ -10,20 +11,19 @@ interface AttachedFileProps {
 
 const AttachedFile: FC<AttachedFileProps> = ({ fileIcon: FileIcon, name }) => {
     return (
-       <div className="w-auto h-[30px] rounded-[20px] py-[7px] pr-[7px] pl-3 gap-[14px]">
+       <div className="w-auto h-[30px] rounded-[20px] py-[7px] pr-[7px] pl-3 gap-[14px] flex justify-between items-center">
            <div id="fileInfo" className="flex justify-between items-center gap-2">
                <FileIcon />
                <span className="font-normal text-[14px] leading-[100%] tracking-normal align-middle text-white">{name}</span>
            </div>
            <div id="ocrInfo" className="flex justify-between items-center gap-1.5">
-               <div>
-                   <Check id="chekOCR" />
+               <div className="flex items-center justify-between gap-1.5">
+                   <Check id="chekOCR" className = "w-5 h-5 rounded-[6px] border-[1.5px] border-[#7C7C7C]"/>
                    <label htmlFor="chekOCR" className="font-normal text-[14px] leading-[100%] tracking-normal align-middle text-[#B7B7B7]">Free OCR </label>
                </div>
                <span><Info/></span>
-               <span>x</span>
            </div>
-
+           <div><Cross/></div>
        </div>
     );
 };
